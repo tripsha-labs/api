d865b2471e4bd5907fa36f6036e38ab5077ab8c3
sanjay madane <madanesanjay@yahoo.co.in>
2019-04-21 01:39:15 +0800

    removed local credentials

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,27 +1,7 @@
 import AWS from "aws-sdk";
-const cred = {
-  region: "localhost",
-  endpoint: "http://localhost:8000",
-  accessKeyId: "AKIA3VUZDZKTRO2F2D7O",
-  secretAccessKey: "RB4ZX7SW4yCXNZKCe3Ew9NKMfLxTuzw"
-};
-
-// import dynamo from "dynamodb";
-// const dynamodb = new AWS.DynamoDB(cred);
-// dynamo.dynamoDriver(dynamodb);
 
 export const executeQuery = (action, params) => {
-  const dynamoDb = new AWS.DynamoDB.DocumentClient(cred);
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
   return dynamoDb[action](params).promise();
 }
-
-// dynamo.createTables({
-//   'User': {readCapacity: 5, writeCapacity: 10}
-// }, (err) => {
-//   if (err) {
-//     console.log('Error creating tables: ', err);
-//   } else {
-//     console.log('Tables has been created');
-//   }
-// });
