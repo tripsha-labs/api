4d275cce4d9c0a1445ea8ee8e760e6562f7cf8d8
sanjay madane <madanesanjay@yahoo.co.in>
2019-04-26 21:32:23 +0800

    [TRIP-52] Added common error handler

--- src/helpers/common.js
+++ src/helpers/common.js
@@ -2,9 +2,11 @@
  * @name - Common
  * @description - All common helpers defined here
  */
+import _ from 'lodash';
+
 export const errorSanitizer = error => {
   try {
-    return [{ type: error.code, message: error.message }];
+    return [{ type: _.camelCase(error.code), message: error.message }];
   } catch (error) {
     return error;
   }
