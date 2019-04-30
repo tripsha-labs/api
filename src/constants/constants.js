099da2c66c4218c209a185c1ec730c18a14bc6bc
sanjay madane <madanesanjay@yahoo.co.in>
2019-04-30 23:10:58 +0800

    Added messages, tags and countries list api

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -8,7 +8,7 @@ export const TABLE_NAMES = {
   TAGS: process.env.tblTags,
   CONNECTIONS: process.env.tblConnections,
   MESSAGES: process.env.tblMessages,
-  GROUP_MESSAGES: process.env.tblGroupMessages,
+  COUNTRIES: process.env.tblCountries,
 };
 
 export const APP_CONSTANTS = {
