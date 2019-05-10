acd2025ba5a3cadb9cd1029df3127326cff16ce2
sanjay madane <madanesanjay@yahoo.co.in>
2019-05-10 22:44:40 +0800

    [TRIP-59] Added countries search support

--- src/routes/countries/list.js
+++ src/routes/countries/list.js
@@ -5,11 +5,31 @@
 import { success, failure, executeQuery } from '../../utils';
 import { TABLE_NAMES, ERROR_CODES } from '../../constants';
 import { errorSanitizer } from '../../helpers';
+import _ from 'lodash';
 
 export const getCountries = async (event, context) => {
-  const params = {
+  let params = {
     TableName: TABLE_NAMES.COUNTRIES,
   };
+  if (event.queryStringParameters && event.queryStringParameters.search) {
+    const nameLower = _.lowerCase(event.queryStringParameters.search);
+    const nameUpper = _.upperCase(event.queryStringParameters.search);
+    params = {
+      ...params,
+      FilterExpression:
+        'begins_with(#name, :nameLower) or begins_with(#name, :nameUpperFirst) or begins_with(#code, :nameUpper)',
+      ExpressionAttributeNames: {
+        '#name': 'name',
+        '#code': 'code',
+      },
+      ExpressionAttributeValues: {
+        ':nameLower': nameLower,
+        ':nameUpperFirst': _.upperFirst(nameLower),
+        ':nameUpper': nameUpper,
+      },
+    };
+  }
+
   try {
     const resCountries = await executeQuery('scan', params);
     return success({
