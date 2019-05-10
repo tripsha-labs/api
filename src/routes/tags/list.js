3a1ac1af48fe59d5b726b2ac25c6a6e2d903c776
sanjay madane <madanesanjay@yahoo.co.in>
2019-05-10 22:43:25 +0800

    [TRIP-58] Added tags search support

--- src/routes/tags/list.js
+++ src/routes/tags/list.js
@@ -5,11 +5,28 @@
 import { success, failure, executeQuery } from '../../utils';
 import { TABLE_NAMES, ERROR_CODES } from '../../constants';
 import { errorSanitizer } from '../../helpers';
+import _ from 'lodash';
 
 export const get = async (event, context) => {
-  const params = {
+  let params = {
     TableName: TABLE_NAMES.TAGS,
   };
+
+  if (event.queryStringParameters && event.queryStringParameters.search) {
+    const nameLower = _.lowerCase(event.queryStringParameters.search);
+    params = {
+      ...params,
+      FilterExpression:
+        'begins_with(#name, :nameLower) or begins_with(#name, :nameUpperFirst)',
+      ExpressionAttributeNames: {
+        '#name': 'name',
+      },
+      ExpressionAttributeValues: {
+        ':nameLower': nameLower,
+        ':nameUpperFirst': _.upperFirst(nameLower),
+      },
+    };
+  }
 
   try {
     const resTags = await executeQuery('scan', params);
