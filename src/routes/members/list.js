d8fe0e5379dae4d0053bcd0f451600da28d1671c
Sanjay Madane <madanesanjay@yahoo.co.in>
2019-05-27 00:01:32 +0800

    [TRIP-73] - Fixed tripId already exists issue in path

--- src/routes/members/list.js
+++ src/routes/members/list.js
@@ -7,14 +7,14 @@ import { success, failure } from '../../utils';
 import { errorSanitizer, getTripMembers } from '../../helpers';
 
 export const memberList = async (event, context) => {
-  if (!(event.pathParameters && event.pathParameters.tripId)) {
+  if (!(event.pathParameters && event.pathParameters.id)) {
     return failure(
-      { ...ERROR_KEYS.MISSING_FIELD, field: 'tripId' },
+      { ...ERROR_KEYS.MISSING_FIELD, field: 'id' },
       ERROR_CODES.VALIDATION_ERROR
     );
   }
   try {
-    const resMembers = await getTripMembers(event.pathParameters.tripId);
+    const resMembers = await getTripMembers(event.pathParameters.id);
     const result = {
       data: resMembers.Items,
       count: resMembers.Count,
