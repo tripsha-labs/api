2b6614621b54a8d50f488cafcd4f0aae32ff5bf7
Sanjay Madane <madanesanjay@yahoo.co.in>
2019-07-03 22:13:45 +0800

    [TRIP-85] Fixed listing specific members messages

--- src/routes/messages/list.js
+++ src/routes/messages/list.js
@@ -3,16 +3,24 @@
  * @description - Message list handler (lambda function)
  */
 import { success, failure, executeQuery } from '../../utils';
-import { TABLE_NAMES, ERROR_CODES } from '../../constants';
+import { TABLE_NAMES, ERROR_CODES, ERROR_KEYS } from '../../constants';
 import { errorSanitizer } from '../../helpers';
 
 export const get = async (event, context) => {
+  if (!(event.queryStringParameters && event.queryStringParameters.memberId)) {
+    return failure(
+      { ...ERROR_KEYS.MISSING_FIELD, field: 'memberId' },
+      ERROR_CODES.VALIDATION_ERROR
+    );
+  }
   const params = {
     TableName: TABLE_NAMES.MESSAGES,
     ExpressionAttributeValues: {
       ':userId': event.requestContext.identity.cognitoIdentityId,
+      ':memberId': event.queryStringParameters.memberId,
     },
-    FilterExpression: 'toMemberId=:userId or fromMemberId=:userId',
+    FilterExpression:
+      '(toMemberId=:userId and fromMemberId=:memberId) or (toMemberId=:memberId and fromMemberId=:userId)',
   };
 
   try {
