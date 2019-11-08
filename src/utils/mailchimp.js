0e43dbf119b1e933f64e90aea28b31b9901c5884
Sanjay Madane <madanesanjay@yahoo.co.in>
2019-11-08 22:33:24 +0800

    [TRIP-270] - subscribe and unsubscribe api added

--- src/utils/mailchimp.js
+++ src/utils/mailchimp.js
@@ -1,11 +1,17 @@
+/**
+ * Mailchimp API utility
+ */
 import axios from 'axios';
-
+import md5 from 'md5';
 const MAILCHIMP_INSTANCE = 'us7';
 const API_KEY = '1ae2fc7c478057aaf8a7568aedf145b8-us7';
-const AUDIENTE_LIST_ID = '885c7a853c';
-const MAILCHIMP_URL = `https://${MAILCHIMP_INSTANCE}.api.mailchimp.com/3.0/lists/${AUDIENTE_LIST_ID}`;
+const AUDIENCE_LIST_ID = '885c7a853c';
 
-export const subscribeUserToMailchimpAudience = async ({ name, email }) => {
+export const subscribeUserToMailchimpAudience = async ({
+  name,
+  email,
+  audienceListId = AUDIENCE_LIST_ID,
+}) => {
   const params = {
     headers: {
       Authorization: `Basic ${API_KEY}`,
@@ -14,6 +20,7 @@ export const subscribeUserToMailchimpAudience = async ({ name, email }) => {
   };
 
   const payload = {
+    update_existing: true,
     members: [
       {
         email_address: email,
@@ -24,10 +31,36 @@ export const subscribeUserToMailchimpAudience = async ({ name, email }) => {
       },
     ],
   };
-
+  const MAILCHIMP_URL = `https://${MAILCHIMP_INSTANCE}.api.mailchimp.com/3.0/lists/${audienceListId}`;
   try {
     const data = await axios.post(MAILCHIMP_URL, payload, params);
-    console.log('Subscribed with success!', data);
+    console.log('Subscribed with success!', data.data);
+  } catch (error) {
+    console.log('error mailchimp:', error.message);
+  }
+};
+
+export const unsubscribeUserToMailchimpAudience = async ({
+  name,
+  email,
+  audienceListId = AUDIENCE_LIST_ID,
+}) => {
+  const params = {
+    headers: {
+      Authorization: `Basic ${API_KEY}`,
+      'Content-Type': 'application/json',
+    },
+  };
+
+  email = md5(email.toLowerCase());
+
+  const payload = {
+    status: 'unsubscribed',
+  };
+  const MAILCHIMP_URL = `https://${MAILCHIMP_INSTANCE}.api.mailchimp.com/3.0/lists/${audienceListId}/members/${email}`;
+  try {
+    const data = await axios.patch(MAILCHIMP_URL, payload, params);
+    console.log('Subscribed with success!', data.data);
   } catch (error) {
     console.log('error mailchimp:', error.message);
   }
