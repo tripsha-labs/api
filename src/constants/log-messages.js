b0444cac47404dc8643eb1270bf414532514a91e
Benjamin Leff <benjamin.w.leff@gmail.com>
2021-12-02 12:11:53 -0700

    feat: [TWA-316] reviwed and changed copy on log-messages.js

--- src/constants/log-messages.js
+++ src/constants/log-messages.js
@@ -1,7 +1,7 @@
 export const LogMessages = {
   CREATE_DRAFT_TRIP_HOST: trip_name => {
     return {
-      message: `Trip ${trip_name} saved as draft.`,
+      message: `Trip ${trip_name} was saved as a draft.`,
       type: 'Trip',
     };
   },
@@ -12,10 +12,10 @@ export const LogMessages = {
     return { message: `You updated ${trip_name}.`, type: 'Trip' };
   },
   DELETE_TRIP_HOST: trip_name => {
-    return { message: `Draft trip ${trip_name} deleted.`, type: 'Trip' };
+    return { message: `Draft trip ${trip_name} was deleted.`, type: 'Trip' };
   },
   TRIP_PUBLISHED: trip_name => {
-    return { message: `Trip ${trip_name} published.`, type: 'Trip' };
+    return { message: `Trip ${trip_name} was published.`, type: 'Trip' };
   },
   BOOKING_REQUEST_TRAVELER: trip_name => {
     return {
@@ -49,7 +49,7 @@ export const LogMessages = {
   },
   BOOKING_REQUEST_EXPIRED_HOST: (traveler_name, trip_name) => {
     return {
-      message: `${traveler_name} booking request for ${trip_name} expired.`,
+      message: `${traveler_name}'s booking request for ${trip_name} expired.`,
       type: 'Booking',
     };
   },
@@ -79,55 +79,55 @@ export const LogMessages = {
   },
   BOOKING_REQUEST_INITIAL_PAYMENT_SUCCESS_HOST: (traveler_name, trip_name) => {
     return {
-      message: `Partial payment for ${traveler_name} booking on ${trip_name} received.`,
+      message: `The partial payment for ${traveler_name}'s booking on ${trip_name} was received.`,
       type: 'Booking',
     };
   },
   BOOKING_REQUEST_INITIAL_PAYMENT_SUCCESS_TRAVELER: trip_name => {
     return {
-      message: `You made partial payment for ${trip_name}.`,
+      message: `You made a partial payment for ${trip_name}.`,
       type: 'Booking',
     };
   },
   BOOKING_REQUEST_FULL_PAYMENT_SUCCESS_HOST: (traveler_name, trip_name) => {
     return {
-      message: `Full payment for ${traveler_name} booking on ${trip_name} received.`,
+      message: `The full payment for ${traveler_name}'s booking on ${trip_name} was received.`,
       type: 'Booking',
     };
   },
   BOOKING_REQUEST_FULL_PAYMENT_SUCCESS_TRAVELER: trip_name => {
     return {
-      message: `You made full payment for ${trip_name}.`,
+      message: `You made a full payment for ${trip_name}.`,
       type: 'Booking',
     };
   },
   BOOKING_REQUEST_INITIAL_PAYMENT_FAILED_HOST: (traveler_name, trip_name) => {
     return {
-      message: `Partial payment for ${traveler_name} booking on ${trip_name} received.`,
+      message: `The partial payment for ${traveler_name}'s booking on ${trip_name} failed.`,
       type: 'Booking',
     };
   },
   BOOKING_REQUEST_INITIAL_PAYMENT_FAILED_TRAVELER: trip_name => {
     return {
-      message: `You made partial payment for ${trip_name}.`,
+      message: `Your partial payment for ${trip_name} failed.`,
       type: 'Booking',
     };
   },
   BOOKING_REQUEST_FULL_PAYMENT_FAILED_HOST: (traveler_name, trip_name) => {
     return {
-      message: `Full payment for ${traveler_name} booking on ${trip_name} received.`,
+      message: `The full payment for ${traveler_name}'s booking on ${trip_name} failed.`,
       type: 'Booking',
     };
   },
   BOOKING_REQUEST_FULL_PAYMENT_FAILED_TRAVELER: trip_name => {
     return {
-      message: `You made full payment for ${trip_name}.`,
+      message: `Your full payment for ${trip_name} failed.`,
       type: 'Booking',
     };
   },
   BOOKING_REQUEST_BALANCE_PAYMENT_SUCCESS_HOST: (traveler_name, trip_name) => {
     return {
-      message: `${traveler_name} balance payment towards ${trip_name} was successful.`,
+      message: `${traveler_name}'s balance payment towards ${trip_name} was successful.`,
       type: 'Booking',
     };
   },
@@ -139,19 +139,19 @@ export const LogMessages = {
   },
   BOOKING_REQUEST_BALANCE_PAYMENT_FAILED_TRAVELER: trip_name => {
     return {
-      message: `Your balance payment towards ${trip_name} was unsuccessful.`,
+      message: `Your balance payment towards ${trip_name} failed.`,
       type: 'Booking',
     };
   },
   BOOKING_REQUEST_BALANCE_PAYMENT_FAILED_HOST: (traveler_name, trip_name) => {
     return {
-      message: `A balance payment by ${traveler_name} towards ${trip_name} was unsuccessful.`,
+      message: `A balance payment by ${traveler_name} towards ${trip_name} failed.`,
       type: 'Booking',
     };
   },
   TRAVELER_LEFT_TRIP_HOST: (traveler_name, trip_name) => {
     return {
-      message: `Your traveler ${traveler_name} has left the ${trip_name}.`,
+      message: `Your traveler, ${traveler_name}, has left the ${trip_name}.`,
       type: 'Membership',
     };
   },
@@ -163,7 +163,7 @@ export const LogMessages = {
   },
   HOST_MEMBER_REMOVAL_REQUEST_HOST: (traveler_name, trip_name) => {
     return {
-      message: `Your request for the removal of ${traveler_name} from your trip ${trip_name} has been sent. You will be notified shortly by the Tripsha Team the status of the request.`,
+      message: `Your request for the removal of ${traveler_name} from your trip ${trip_name} has been sent. You will be notified shortly by the Tripsha Team of the status of the request.`,
       type: 'Membership',
     };
   },
@@ -172,28 +172,19 @@ export const LogMessages = {
     trip_name
   ) => {
     return {
-      message: `Your request for the removal ${traveler_name} traveler has been approved. This traveler is no longer attending ${trip_name}.`,
-      type: 'Membership',
-    };
-  },
-  TRAVELER_MEMBER_REMOVAL_REQUEST_APPROVED_HOST: (
-    traveler_name,
-    trip_name
-  ) => {
-    return {
-      message: `Your request for the removal ${traveler_name} traveler has been approved. This traveler is no longer attending ${trip_name}.`,
+      message: `Your request to remove ${traveler_name} has been approved. This traveler is no longer attending ${trip_name}.`,
       type: 'Membership',
     };
   },
   TRAVELER_ADDED_IN_TRIP_BY_HOST: trip_name => {
     return {
-      message: `You have been added to  ${trip_name} by the host.`,
+      message: `You have been added to ${trip_name} by the host.`,
       type: 'Membership',
     };
   },
   HOST_LINKED_TO_STRIPE_ACCOUNT: () => {
     return {
-      message: `You have successfully been linked to a Stripe account with Tripsha.`,
+      message: `You have successfully linked your Stripe account to Tripsha.`,
       type: 'StripeConnect',
     };
   },
