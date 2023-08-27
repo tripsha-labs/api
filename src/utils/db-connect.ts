45d3a785227dbee7d8fb7d1aa8de3d8cde3833c3
kanelabs <ryan@kanelabs.com>
2023-08-27 12:47:07 -0500

    Add thorough testing strategy

--- src/utils/db-connect.ts
+++ src/utils/db-connect.ts
@@ -6,14 +6,13 @@ import mongoose from 'mongoose';
 
 export const dbConnect = async (uri: string) => {
   try {
-    await mongoose.connect(uri, {
+    return await mongoose.connect(uri, {
       ssl: true,
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
       poolSize: 10,
     });
-    console.log('DB connected');
   } catch (error) {
     console.log('DB connection error', error);
   }
